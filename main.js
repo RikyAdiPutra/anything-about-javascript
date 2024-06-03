0d08dab798fe4a3e7b6a46e3f2e904b5d383b392
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-06-03 23:49:41 +0700

    dynamic button

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
 const myButton = document.querySelector("button");
-const myHeading = document.querySelector("h1");
 
-function setUserName() {
-  const myName = prompt("Please enter your name");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Hello, welcome ${myName}`;
-  }
-}
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Halo brooo ${storedName}`;
-}
-
-myButton.onclick = () => {
-  setUserName();
-};
+myButton.addEventListener("click", () => {
+  const newName = prompt("Enter your name");
+  myButton.textContent = `Player 1 : ${newName}`;
+});
